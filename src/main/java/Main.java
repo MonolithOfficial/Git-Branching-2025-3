36592b05e012daff03eafadb16ebdebc7af6651c
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2025-11-18 19:51:01 +0400

    added doNotDoSomething method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Added main function");
     }
+
+    public static void doNotDoSomething(){
+        System.out.println("Something was not done.");
+    }
 }
